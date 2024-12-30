ce717f16c317e64bacae4a79342fd27da3377380
TheLazySol <tbelonog25@gmail.com>
2024-12-29 21:48:11 -0500

    Set-up project structure

--- sos-programs/common/src/lib.rs
+++ sos-programs/common/src/lib.rs
@@ -2,12 +2,13 @@ use anchor_lang::prelude::*;
 
 declare_id!("85aDWaYQbL9X67aHgpJbkTzYshzKe5VjnyjMuJ351Fc9");
 
+
 #[program]
-pub mod sos_programs {
+pub mod hello_world {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Hello, World!");
         Ok(())
     }
 }
