6a8d8c9f18d60eeb46d8fcf97bb461b7fc7908d5
TheLazySol <tbelonog25@gmail.com>
2025-01-03 13:22:21 -0500

    types.rs adjusted & refactored / utils.rs has been set up for calculations to be added / Cargo.toml added dependancies

--- sos-programs/common/src/utils.rs
+++ sos-programs/common/src/utils.rs
@@ -2,36 +2,65 @@ use anchor_lang::prelude::*;
 
 use common::types::{OptionDataTypes, MarketDataTypes};
 
-
 pub mod black_scholes_model {
-    pub mod standard_normal_variables {
-        use super::*;
+    use crate::types::{OptionDataTypes, TokenDataTypes, MarketDataTypes};
 
-        pub fn calc_d_plus(
-            // TODO
-        ) -> f64 {
-            // TODO
-        }
-
-        pub fn calc_d_minus(
-            // TODO
-        ) -> f64 {
-            // TODO
-        }
+    /// Calculates the option price using Black-Scholes model
+    pub fn calculate_option_price(
+        option_params: &OptionDataTypes::OptionParams,
+        token_params: &TokenDataTypes::TokenParams,
+        market_params: &MarketDataTypes::MarketParams,
+    ) -> f64 {
+        // TODO: Implement Black-Scholes formula
     }
 
-    pub mod greeks {
-        use super::*;
-        // Calculate Delta 
-        pub fn calc_delta(
-            token_price: MarketDataTypes::TokenPrice,
-            token_volatility: MarketDataTypes::TokenVolatility,
-            time_in_years: MarketDataTypes::TimeInYears,
-            risk_free_rate: MarketDataTypes::RiskFreeRate,
-            strike_price: OptionDataTypes::StrikePrice,
-        ) -> OptionDataTypes::OptionGreeks::delta {
-            // TODO
-        }
+    /// Calculates time to expiry in years
+    pub fn calculate_time_to_expiry(
+        initial_expiry: i64,
+        current_timestamp: i64,
+    ) -> f64 {
+        // TODO: Convert time difference to years
+    }
 
+    /// Calculates option greeks
+    pub fn calculate_greeks(
+        option_params: &OptionDataTypes::OptionParams,
+        token_params: &TokenDataTypes::TokenParams,
+        market_params: &MarketDataTypes::MarketParams,
+    ) -> OptionDataTypes::OptionGreeks {
+        // TODO: Calculate delta, gamma, theta, vega, rho
+    }
+
+    /// Checks if option is expired
+    pub fn is_option_expired(
+        initial_expiry: i64,
+        current_timestamp: i64,
+    ) -> bool {
+        // TODO: Check expiration
+    }
+
+    /// Helper function for cumulative normal distribution
+    fn normal_cdf(x: f64) -> f64 {
+        // TODO: Implement N(x) function
+    }
+
+    /// Calculate d1 parameter for Black-Scholes
+    fn calculate_d1(
+        spot_price: f64,
+        strike_price: f64,
+        risk_free_rate: f64,
+        volatility: f64,
+        time_to_expiry: f64,
+    ) -> f64 {
+        // TODO: Implement d1 calculation
+    }
+
+    /// Calculate d2 parameter for Black-Scholes
+    fn calculate_d2(
+        d1: f64,
+        volatility: f64,
+        time_to_expiry: f64,
+    ) -> f64 {
+        // TODO: Implement d2 calculation
     }
 }
