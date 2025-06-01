2d9332b2a5a320225aee58a50ffdaf37989708b6
TheLazySol <tbelonog25@gmail.com>
2025-06-01 19:39:15 -0400

    Remove obsolete files and directories, including Anchor.toml, Cargo.toml, and various source files related to the option_create and common modules. Update LICENSE to reflect new copyright year. This cleanup prepares the project for a more streamlined structure and future development.

--- sos_programs/option_create/src/constants.rs
+++
@@ -1,6 +0,0 @@
-pub const SECONDS_IN_YEAR: i64 = 31_536_000;
-pub const MIN_OPTION_DURATION: i64 = 86_400; // 1 day
-pub const MAX_OPTION_DURATION: i64 = 31_536_000; // 1 year
-pub const MAX_VOLATILITY: f64 = 1.0;
-pub const MIN_VOLATILITY: f64 = 0.01;
-pub const MIN_STRIKE_PRICE: f64 = 0.000001;
